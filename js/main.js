042885400e298b8e4dc95058abacf0ed5c5dede5
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-02-28 22:18:14 +0000

    Completed view swap function, and built hamburger menu. #1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+// function viewSwap(newView) {
+//   if (data.view !== newView) {
+//     var $newView = document.querySelector('[data-view="' + newView + '"]');
+//     var $oldView = document.querySelector('[data-view="' + data.view + '"]');
+//     $newView.classList.remove('hidden');
+//     $oldView.classList.add('hidden');
+//     data.view = newView;
+//   }
+// }
