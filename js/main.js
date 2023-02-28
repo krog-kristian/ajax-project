e6e035dd64e152db573e0baf6255a6f447118dc5
Kristian Krog <118767169+krog-kristian@users.noreply.github.com>
2023-02-28 22:40:35 +0000

    Completed hamburger menu and view swapping. #1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
+function viewSwap(newView) {
+  if (data.view !== newView) {
+    var $newView = document.querySelector('[data-view="' + newView + '"]');
+    var $oldView = document.querySelector('[data-view="' + data.view + '"]');
+    $newView.classList.remove('hidden');
+    $oldView.classList.add('hidden');
+    data.view = newView;
+  }
+}
 
-// function viewSwap(newView) {
-//   if (data.view !== newView) {
-//     var $newView = document.querySelector('[data-view="' + newView + '"]');
-//     var $oldView = document.querySelector('[data-view="' + data.view + '"]');
-//     $newView.classList.remove('hidden');
-//     $oldView.classList.add('hidden');
-//     data.view = newView;
-//   }
-// }
+var $hamburgerButton = document.querySelector('#hamburger');
+
+$hamburgerButton.addEventListener('click', function () {
+  if ($hamburger.classList.contains('hidden')) {
+    $hamburger.classList.remove('hidden');
+  } else {
+    $hamburger.classList.add('hidden');
+  }
+});
+
+var $hamburger = document.querySelector('#hamburger-open');
+
+$hamburger.addEventListener('click', function () {
+  if (event.target.closest('li')) {
+    var $targetLi = event.target.closest('li');
+    var newView = $targetLi.getAttribute('data-view');
+    viewSwap(newView);
+    $hamburger.classList.add('hidden');
+  }
+});
